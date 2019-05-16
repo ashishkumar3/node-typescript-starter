6bc9fc920abdd3fdd383ad934f851d16fddc05fb
ashish <ashishk4371@gmail.com>
2019-05-16 21:40:25 +0530

    ejs, dotenv added

--- dist/index.js
+++ dist/index.js
@@ -3,10 +3,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
+// initialize configuration
+dotenv_1.default.config();
 const app = express_1.default();
+const PORT = process.env.SERVER_PORT;
+// configure express to use ejs
+app.set("views", path_1.default.join(__dirname + "/views"));
+app.set("view engine", "ejs");
 app.get("/", (req, res) => {
-    res.send("Hello, World fellas");
+    // render index template
+    res.render("index");
 });
-app.listen(3000);
+app.listen(PORT, () => {
+    // tslint:disable-next-line:no-console
+    console.log(`Server running on Port: ${PORT}`);
+});
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
+import dotenv from "dotenv";
 import express from "express";
+import path from "path";
+
+// initialize configuration
+dotenv.config();
 
 const app = express();
 
-app.get("/", (req: any, res: any) => {
+const PORT = process.env.SERVER_PORT;
 
-  res.send("Hello, World fellas");
+// configure express to use ejs
+app.set("views", path.join(__dirname + "/views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req: any, res: any) => {
+  // render index template
+  res.render("index");
 });
 
-app.listen(3000);
+app.listen(PORT, () => {
+  // tslint:disable-next-line:no-console
+  console.log(`Server running on Port: ${PORT}`);
+});
